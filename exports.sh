82f06baffe303eb1204d4992c17e2767909b13e3
Nuthan Chandra <nuthanc@nodem4.englab.juniper.net>
2019-09-05 15:57:29 +0530

    Add comment

--- exports.sh
+++ exports.sh
@@ -9,11 +9,13 @@ export CONTAINER_REGISTRY_PASSWORD=FSg0vLW^7oM#GZy8Ju*f
 export VIRTUAL_SETUP=1 # Make this 0 if nodeg12 setup is used
 
 export COMMAND_SERVERS_FILE=/root/deployment/command_servers.yml
+
 if [ $VIRTUAL_SETUP -eq 1 ]
 then 
     export INSTANCES_FILE=/root/deployment/instances_virtual.yml
 else
-    export INSTANCES_FILE=/root/deployment/instances.yml
+    # Change this to instances_mgmt.yml if management ip needs to be used
+    export INSTANCES_FILE=/root/deployment/instances.yml 
 fi
 # Set both below as 0 when No Action is required
 export PROVISION=1
