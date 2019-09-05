39bda494ec8c83c8bafa58e3b273ea01144971cc
Nuthan Chandra <nuthanc@nodem4.englab.juniper.net>
2019-09-05 16:04:03 +0530

    Change deployment to k8s_contrail_command_deployment

--- exports.sh
+++ exports.sh
@@ -8,14 +8,14 @@ export CONTAINER_REGISTRY_PASSWORD=FSg0vLW^7oM#GZy8Ju*f
 
 export VIRTUAL_SETUP=1 # Make this 0 if nodeg12 setup is used
 
-export COMMAND_SERVERS_FILE=/root/deployment/command_servers.yml
+export COMMAND_SERVERS_FILE=/root/k8s_contrail_command_deployment/command_servers.yml
 
 if [ $VIRTUAL_SETUP -eq 1 ]
 then 
-    export INSTANCES_FILE=/root/deployment/instances_virtual.yml
+    export INSTANCES_FILE=/root/k8s_contrail_command_deployment/instances_virtual.yml
 else
     # Change this to instances_mgmt.yml if management ip needs to be used
-    export INSTANCES_FILE=/root/deployment/instances.yml 
+    export INSTANCES_FILE=/root/k8s_contrail_command_deployment/instances.yml 
 fi
 # Set both below as 0 when No Action is required
 export PROVISION=1
