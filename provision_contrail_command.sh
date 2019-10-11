bd29abeb6f972d894fc1c22f663ca45b80957bfe
nuthanc <nuthanc@juniper.net>
2019-10-11 13:49:20 +0530

    Remove -d to run in fg

--- provision_contrail_command.sh
+++ provision_contrail_command.sh
@@ -85,10 +85,10 @@ docker pull $CCD_IMAGE
 
 if [ $PROVISION -eq 1 ]
 then
-    docker run -t --net host -e orchestrator=kubernetes -e action=provision_cluster -v $COMMAND_SERVERS_FILE:/command_servers.yml -v $INSTANCES_FILE:/instances.yml -d --privileged --name contrail_command_deployer $CCD_IMAGE
+    docker run -t --net host -e orchestrator=kubernetes -e action=provision_cluster -v $COMMAND_SERVERS_FILE:/command_servers.yml -v $INSTANCES_FILE:/instances.yml --privileged --name contrail_command_deployer $CCD_IMAGE
 elif [ $IMPORT -eq 1 ]
 then
-    docker run -t --net host -e orchestrator=kubernetes -e action=import_cluster -v $COMMAND_SERVERS_FILE:/command_servers.yml -v $INSTANCES_FILE:/instances.yml -d --privileged --name contrail_command_deployer $CCD_IMAGE
+    docker run -t --net host -e orchestrator=kubernetes -e action=import_cluster -v $COMMAND_SERVERS_FILE:/command_servers.yml -v $INSTANCES_FILE:/instances.yml --privileged --name contrail_command_deployer $CCD_IMAGE
 else
     docker run -td --net host -v $COMMAND_SERVERS_FILE:/command_servers.yml --privileged --name contrail_command_deployer $CCD_IMAGE
 fi
