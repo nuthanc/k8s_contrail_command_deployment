f9ff88cb59048051932f15eeb594d7639dd4e48a
nuthanc <nuthanc@juniper.net>
2019-10-13 12:49:15 +0530

    Add 1m for testing

--- jenkins_start.sh
+++ jenkins_start.sh
@@ -10,8 +10,8 @@ then
     server-manager reimage --no_confirm --server_id nodec58 centos-7.7
     server-manager reimage --no_confirm --server_id nodec60 centos-7.7
     server-manager reimage --no_confirm --server_id nodec61 centos-7.7
-    
-    sleep 15m
+
+    sleep 1m
 
     server-manager status server --cluster_id k8s_nodeg12_ha_setup|grep "reimage_started\|restart_issued"
 
