e1eaba392d749eceab3d609817bf1683c87b2dfd
nuthanc <nuthanc@juniper.net>
2019-10-13 12:48:45 +0530

    Add the instructions within the sshpass

--- jenkins_start.sh
+++ jenkins_start.sh
@@ -10,9 +10,8 @@ then
     server-manager reimage --no_confirm --server_id nodec58 centos-7.7
     server-manager reimage --no_confirm --server_id nodec60 centos-7.7
     server-manager reimage --no_confirm --server_id nodec61 centos-7.7
-    )"
-
-    sleep 1m
+    
+    sleep 15m
 
     server-manager status server --cluster_id k8s_nodeg12_ha_setup|grep "reimage_started\|restart_issued"
 
@@ -21,6 +20,7 @@ then
         sleep 10
         server-manager status server --cluster_id k8s_nodeg12_ha_setup|grep "reimage_started\|restart_issued"
     done
+    )"
 fi
 
 # Add nodea4 ssh-pass 
