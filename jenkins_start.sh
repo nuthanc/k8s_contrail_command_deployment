2758fa3eceb4405cacc05c436b578f9bef55bc36
nuthanc <nuthanc@juniper.net>
2019-10-13 12:15:05 +0530

    Add command again in while loop

--- jenkins_start.sh
+++ jenkins_start.sh
@@ -19,6 +19,7 @@ then
     while [ $? -eq 0 ]
     do
         sleep 10
+        server-manager status server --cluster_id k8s_nodeg12_ha_setup|grep "reimage_started\|restart_issued"
     done
 fi
 
