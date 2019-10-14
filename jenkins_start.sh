7ff470ff40e0c6d84474cfd870fb044362731e4b
nuthanc <nuthanc@juniper.net>
2019-10-14 11:48:46 +0530

    Remove unwanted command

--- jenkins_start.sh
+++ jenkins_start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -ex
 
-sshpass -p 'c0ntrail123' ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -l root $SANITY_NODE "bash -s" <./run_sanity.sh
-
 sshpass -p 'c0ntrail123' ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -l root $SANITY_NODE "(
     set -ex
     kubectl patch deployment coredns -n kube-system -p '{"spec":{"template":{"spec":{"containers":[{"name":"coredns", "image":"coredns/coredns:1.2.6"}]}}}}'
