9175dbff6589e3351b206aac20a0a58142af2c52
nuthanc <nuthanc@juniper.net>
2019-10-14 11:58:01 +0530

    Change sshpass format

--- jenkins_start.sh
+++ jenkins_start.sh
@@ -1,3 +1,17 @@
 #!/bin/bash -ex
 
-cat run_sanity.sh | sshpass -p 'c0ntrail123' ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -l root $SANITY_NODE 
+sshpass -p 'c0ntrail123' ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -l root $SANITY_NODE <<EOF
+kubectl patch deployment coredns -n kube-system -p '{"spec":{"template":{"spec":{"containers":[{"name":"coredns", "image":"coredns/coredns:1.2.6"}]}}}}'
+
+if [ $INSECURE -eq 0 ]
+then
+    docker run --entrypoint /bin/bash --network=host -it hub.juniper.net/contrail-nightly/contrail-test-test:$CONTRAIL_VERSION
+else
+    docker run --entrypoint /bin/bash --network=host -it 10.204.217.152:5000/contrail-test-test:$CONTRAIL_VERSION
+fi
+sshpass -p c0ntrail123 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r root@${COMMAND_SERVER_IP}:${INSTANCES_FILE} /contrail-test/contrail_test_input.yaml
+export PYTHONPATH=./scripts:./fixtures TEST_CONFIG_FILE=contrail_test_input.yaml
+export MX_GW_TEST=1
+export EMAIL_SUBJECT=Micro-services-Multi-Interface-Kubernetes-HA-Sanity-Ansible
+bash -x run_tests.sh -m -U -T k8s_sanity -t
+EOF
