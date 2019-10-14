a307238127695b14b20159c083e9650d8f678418
nuthanc <nuthanc@juniper.net>
2019-10-14 17:33:16 +0530

    Comment MX_GW_TEST

--- jenkins_start.sh
+++ jenkins_start.sh
@@ -60,7 +60,7 @@ fi
 set -x
 sshpass -p c0ntrail123 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r root@${COMMAND_SERVER_IP}:${INSTANCES_FILE} /contrail-test/contrail_test_input.yaml
 export PYTHONPATH=./scripts:./fixtures TEST_CONFIG_FILE=contrail_test_input.yaml
-export MX_GW_TEST=1
+# export MX_GW_TEST=1
 export EMAIL_SUBJECT=Micro-services-Multi-Interface-Kubernetes-HA-Sanity-Ansible
 cd contrail-test
 bash -x run_tests.sh -m -U -T k8s_sanity -t
