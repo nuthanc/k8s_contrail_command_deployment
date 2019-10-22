9332ebe219f4e5ff906b70ebcd0c43fd3f8ddb0c
nuthanc <nuthanc@juniper.net>
2019-10-23 00:13:58 +0530

    Update run_sanity file for running manually

--- run_with_delay.sh
+++ run_with_delay.sh
@@ -1,4 +1,12 @@
 #!/bin/sh -x
 
 sleep $1m
-source jenkins_start.sh
+
+export SANITY_NODE=10.204.217.52
+export INSECURE=1
+export INSECURE_REGISTRY=10.84.5.81:5010
+export CONTRAIL_VERSION=1911.1
+export COMMAND_SERVER_IP=10.204.216.120
+export INSTANCES_FILE=/root/Nuthan_jenkins/instances.yml
+
+source run_sanity.sh
