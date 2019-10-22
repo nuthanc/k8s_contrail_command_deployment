84bbb904e744e18e55e2d4ae6f482f5b3ebb24fc
nuthanc <nuthanc@juniper.net>
2019-10-22 20:42:50 +0530

    Store 0 in status

--- reimage.sh
+++ reimage.sh
@@ -8,9 +8,11 @@ server-manager reimage --no_confirm --server_id nodec61 centos-7.7
 sleep 14.5m
 
 server-manager status server --cluster_id k8s_nodeg12_ha_setup|grep "reimage_started\|restart_issued"
+status=$?
 
-while [ $? -eq 0 ]
+while [ $status -eq 0 ]
 do
     sleep 10
     server-manager status server --cluster_id k8s_nodeg12_ha_setup|grep "reimage_started\|restart_issued"
+    status=$?
 done
