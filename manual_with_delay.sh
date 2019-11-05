78693d82f79b66ee9e7f8ce1014631e06e264051
nuthanc <nuthanc@juniper.net>
2019-11-05 11:24:32 +0530

    For virtual

--- manual_with_delay.sh
+++ manual_with_delay.sh
@@ -2,14 +2,15 @@
 
 sleep $1m
 # nodeg12 10.204.217.52     and    vm1 10.204.218.103
-export SANITY_NODE=10.204.217.52
+export SANITY_NODE=10.204.218.103
+# Make INSECURE 0 for hub.juniper.net and 1 for insecure along with its registry
 export INSECURE=1
 export INSECURE_REGISTRY=10.204.217.152:5000
-export CONTRAIL_VERSION=1911.8
+export CONTRAIL_VERSION=1911.14
 # nodea4 10.204.216.120     and     nodea35 10.204.216.31
-export COMMAND_SERVER_IP=10.204.216.120
-# Use instances_virtual.yml with nodea35 for virtual setup
-export INSTANCES_FILE=/root/Nuthan_jenkins/instances.yml
+export COMMAND_SERVER_IP=10.204.216.31
+# Use instances_virtual.yml with nodea35 for virtual setup and instances.yml for physical setup
+export INSTANCES_FILE=/root/Nuthan_jenkins/instances_virtual.yml
 
 source run_sanity.sh
 
--- run_sanity.sh
+++ run_sanity.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -ex
 
 sshpass -p 'c0ntrail123' ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -l root $SANITY_NODE <<EOF
-kubectl patch deployment coredns -n kube-system -p '{"spec":{"template":{"spec":{"containers":[{"name":"coredns", "image":"coredns/coredns:1.2.6"}]}}}}'
+# kubectl patch deployment coredns -n kube-system -p '{"spec":{"template":{"spec":{"containers":[{"name":"coredns", "image":"coredns/coredns:1.2.6"}]}}}}'
 
 if [ $INSECURE -eq 0 ]
 then
@@ -11,7 +11,7 @@ else
 fi
 sshpass -p c0ntrail123 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r root@${COMMAND_SERVER_IP}:${INSTANCES_FILE} /contrail-test/contrail_test_input.yaml
 export PYTHONPATH=./scripts:./fixtures TEST_CONFIG_FILE=contrail_test_input.yaml
-export MX_GW_TEST=0
+export MX_GW_TEST=1
 export EMAIL_SUBJECT=Micro-services-Multi-Interface-Kubernetes-HA-Sanity-Ansible
 cd contrail-test
 bash -x run_tests.sh -m -U -T k8s_sanity -t
