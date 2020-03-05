d07fc9d95ee2168f35bcac822c7e1a733e46a562
nuthanc <nuthanc@juniper.net>
2020-03-05 14:24:27 +0530

    Add export_and_source file

--- jenkins_start.sh
+++ jenkins_start.sh
@@ -21,31 +21,8 @@ sshpass -p 'c0ntrail123' ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/
 
 sshpass -p 'c0ntrail123' scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r $WORKSPACE/ root@${COMMAND_SERVER_IP}:/root/Nuthan_jenkins
 
-sshpass -p 'c0ntrail123' ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -l root $COMMAND_SERVER_IP <<EOF
-
-    export WORKSPACE=$WORKSPACE
-    export INSECURE=$INSECURE 
-    export TAG=$TAG 
-    export CONTRAIL_VERSION=$CONTRAIL_VERSION
-    export COMMAND_SERVER_IP=$COMMAND_SERVER_IP
-    export CONTAINER_REGISTRY_USERNAME=$CONTAINER_REGISTRY_USERNAME
-    export CONTAINER_REGISTRY_PASSWORD=$CONTAINER_REGISTRY_PASSWORD
-    export PROVISION=$PROVISION
-    export IMPORT=$IMPORT
-    export VIRTUAL_SETUP=$VIRTUAL_SETUP 
-    export REIMAGE=$REIMAGE
-    export COMMAND_SERVERS_FILE=$COMMAND_SERVERS_FILE
-    export INSTANCES_FILE=$INSTANCES_FILE
-    export WORKING_DIR=$WORKING_DIR
-
-    set -x
-    docker stop $(docker ps -a -q) 
-    docker system prune -f
-    docker rmi $(docker images -a -q)
-    
-    source ${WORKING_DIR}/exports.sh
-    source ${WORKING_DIR}/provision_contrail_command.sh
-EOF
+sshpass -p 'c0ntrail123' ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -l root $COMMAND_SERVER_IP "bash -s" <./reimage.sh
+sshpass -p 'c0ntrail123' ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -l root $COMMAND_SERVER_IP <./export_and_source.sh
 
 # source exports.sh
 # source provision_contrail_command.sh
