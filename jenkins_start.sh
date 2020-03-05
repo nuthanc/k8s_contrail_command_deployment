27078efc481ae4d7031ad140d2ff45ba8d2093ac
nuthanc <nuthanc@juniper.net>
2020-03-05 22:22:26 +0530

    Revert "Add Or statement of true"
    This reverts commit 8a77c30a10fa56dcaf909cf85770ebb23f733eb9.

--- jenkins_start.sh
+++ jenkins_start.sh
@@ -39,9 +39,9 @@ sshpass -p 'c0ntrail123' ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/
     export WORKING_DIR=$WORKING_DIR
 
     set -x
-    docker stop $(docker ps -a -q) || true
+    docker stop $(docker ps -a -q) 
     docker system prune -f
-    docker rmi $(docker images -a -q) || true
+    docker rmi $(docker images -a -q)
     
     source ${WORKING_DIR}/exports.sh
     source ${WORKING_DIR}/provision_contrail_command.sh
